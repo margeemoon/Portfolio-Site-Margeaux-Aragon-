594d4b899abff498f723a029bb17f77e4390b7f2
margeemoon <79113493+margeemoon@users.noreply.github.com>
2021-05-03 23:41:29 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-console.log("Hi There, Welcome to My Portfolio")
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
