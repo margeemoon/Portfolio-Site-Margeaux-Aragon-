77c760f0a8bb69e3d0737ab42feee6cddebf529b
margeemoon <79113493+margeemoon@users.noreply.github.com>
2021-05-23 21:29:17 -0700

    added jquery, added hamburger menu, used js to create interactivity with contact and hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-<script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous"></script>
+< script src = "https://code.jquery.com/jquery-3.3.1.min.js"
+integrity = "sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+crossorigin = "anonymous" > < /script> <
+script src = "js/scripts.js" > < /script>
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
